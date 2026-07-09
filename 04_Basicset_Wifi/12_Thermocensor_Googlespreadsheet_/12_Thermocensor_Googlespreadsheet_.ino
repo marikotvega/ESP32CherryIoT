b3c7c54aff10540709d185b9606c5651d2e02f21
marikotvega <marikotvega@gmail.com>
2026-07-09 16:13:45 +0900

    My Thermo sensor to spreadsheet

---
+++ 04_Basicset_Wifi/12_Thermocensor_Googlespreadsheet_/12_Thermocensor_Googlespreadsheet_.ino
@@ -0,0 +1,118 @@
+//https://www.haruirosoleil.com/entry/2020/02/02/101937
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <DHT20.h> //DHT20 by RobTillaart
+#include <Wire.h>
+
+DHT20 DHT;
+
+// 接続先のSSIDとパスワード
+const char* ssid = "XXXX"; //無線ルーターのssidを入力
+const char* password = "XXXX"; //無線ルーターのパスワードを入力
+
+const int litsnsrPin = 3; //3:ConnectorA 4:ConnectorB
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(litsnsrPin, INPUT);
+  Wire.begin(1, 3); //1,3:ConnectorA 5,4:ConnectorB
+}
+
+
+void loop() {
+  sendData();
+  delay(10000);
+}
+
+
+//WiFiに接続
+void connectWiFi(){
+
+  Serial.print("ssid:");
+  Serial.print(ssid);
+  Serial.println(" に接続します。");
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("WiFiに接続中");
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("接続しました。");
+
+  //IPアドレスの表示
+  Serial.print("IPアドレス:");
+  Serial.println(WiFi.localIP());
+}
+
+ 
+
+//測定データの送信
+void sendData(){
+
+  WiFiClientSecure sslclient;
+
+  const char* server = "script.google.com";
+//  String url = "https://script.google.com/macros/s/xxxxxxxxxxxxxx/exec";  //googlescript web appのurlを入力
+  String url = "https://script.google.com/macros/s/AKfycbyDT1GE-lTbSD6SHS2RpRMjuESWhjq4zlrsFSyV1Aa71P5bOEelzMWOUHFMGrb1Qv4G/exec";
+
+  //測定値を準備
+  //float litsnsr_ad = analogRead(litsnsrPin); // Read analog data
+  //float litsnsr_v = litsnsr_ad * 3.3 / 4096; // Calculation of voltage value
+  //float lux = 10000 * litsnsr_v / (3.3 - litsnsr_v) / 1000; // Calculation of lux value
+
+  DHT.read();
+  float Temperature = DHT.getTemperature();
+  float Humidity = DHT.getHumidity();
+  //Look at the serial monitor
+  Serial.println(String(Humidity) + "％");
+  Serial.print(" / ");
+  Serial.print(String(Temperature) + "℃");
+  
+  delay(500);
+  //  Serial.print(lux);
+  //  Serial.println(" Lux ");
+  
+ // float sensor_data1= lux;
+
+  //wifiに接続
+  connectWiFi();
+
+  //測定値の表示
+ // Serial.println(sensor_data1);
+  
+
+  //urlの末尾に測定値を加筆
+  url += "?";
+  url += "&1_cell=";
+  url += String(Humidity);
+  url += "&2_cell=";
+  url += String(Temperature);
+ 
+  Serial.println(url);
+  // サーバーにアクセス
+  Serial.println("サーバーに接続中...");
+  sslclient.setInsecure(); //skip verification
+
+  //データの送信
+  if (!sslclient.connect(server, 443)) {
+    Serial.println("接続に失敗しました");
+    Serial.println("");//改行
+    return;
+  }
+
+  Serial.println("サーバーに接続しました");
+
+  sslclient.println("GET " + url);
+  delay(1000); //私の環境ではここに待ち時間を入れないとデータが送れないことがある
+  sslclient.stop();
+
+  Serial.println("データ送信完了");
+  Serial.println("");//改行
+
+  //WiFiを切断
+  WiFi.mode(WIFI_OFF);
+}
